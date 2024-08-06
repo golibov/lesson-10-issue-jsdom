b67a6bea02e8d3c0d4832ff58f1fb85c6aff0ef1
golibov <golibovshg7@gmail.com>
2024-08-07 01:50:05 +0500

    the end

---
+++ js/app.js
@@ -0,0 +1,48 @@
+"use strict";
+
+// let btnClick = document.querySelector(".click");
+// let body = document.querySelector("body");
+
+
+// let color = new webkitSpeechRecognition();
+// color.lang = "EN-en";
+
+// color.onresult = function (e) {
+//     let transcript = e.results[0][0].transcript.trim().toLowerCase();
+//     if (transcript === "red") {
+//         document.body.style.backgroundColor = "red";
+//         console.log("Fon rangi qizilga o'zgartirildi");
+//     } else if (transcript === "green") {
+//         document.body.style.backgroundColor = "green";
+//         console.log("Fon yashilga  o'zgartirildi");
+//     } else if (transcript === "blue") {
+//         document.body.style.backgroundColor = "blue";
+//         console.log("Fon ko'kga  o'zgartirildi");
+//     } else if (transcript === "yellow") {
+//         document.body.style.backgroundColor = "yellow";
+//         console.log("Fon sariqga  o'zgartirildi");
+//     } else if (transcript === "violet") {
+//         document.body.style.backgroundColor = "violet";
+//         console.log("Fon binafshaga  o'zgartirildi");
+//     } else if (transcript === "aqua") {
+//         document.body.style.backgroundColor = "aqua"
+//         console.log("Fon aqua ga o'zgartirildi");
+//         if (document.body.style.backgroundColor === "aqua") {
+//             btnClick.style.backgroundColor = "blue";
+//             console.log("Fon rangi aqua bo'lsa, tugma rangi blue ga o'zgartirildi");
+//         }
+//     } else {
+//         console.log("Bunday ranglarni bilmas ekanman");
+//     }
+
+
+
+// };
+
+// function clickBtn() {
+//     color.start();
+// }
+
+
+
+
